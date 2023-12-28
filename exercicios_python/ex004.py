8599db9f34c9287a46d35ef004569a402497e7c4
Davi <davialvesdeelimaa@gmail.com>
2023-12-28 18:14:32 -0300

    adicionando arquivo

---
+++ exercicios_python/ex004.py
@@ -0,0 +1,5 @@
+texto = input('digite algo: ')
+print('Tipo primitivo desse valor é:', type(texto))
+print('É numerico ', texto.isnumeric())
+print('É maiuculo', texto.isupper())
+print('É minusculo', texto.islower())
