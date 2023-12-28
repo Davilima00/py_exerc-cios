3794a9cc66a39ceb290f15022de4fc42e6cd972d
Davi <davialvesdeelimaa@gmail.com>
2023-12-28 18:29:01 -0300

    adicionando arquivo

---
+++ exercicios_python/ex005.py
@@ -0,0 +1,4 @@
+numero = int(input('digite um numero '))
+antecessor = numero -1 
+sucessor = numero + 1 
+print(f'Analizando o valor {numero}, seu antecessor e {antecessor} seu sucessor e {sucessor}')
