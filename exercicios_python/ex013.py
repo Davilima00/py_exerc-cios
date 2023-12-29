881735683851eaf67d48d320b6fb57096c3b9524
Davi <davialvesdeelimaa@gmail.com>
2023-12-29 01:00:16 -0300

    reajuste salarial

---
+++ exercicios_python/ex013.py
@@ -0,0 +1,3 @@
+salario = float(input('qual e o salario do fucionario '))
+reajuste = salario-(salario*(15/100))
+print(f"um fucionario que ganhava {salario :.2f}, com 15 pocento de desconto passa a receber{ reajuste} ")
