6546778906690282f63d45eecc2e5ee6cfcd2931
Davi <davialvesdeelimaa@gmail.com>
2023-12-29 13:27:32 -0300

    musica mp3

---
+++ exercicios_python/ex021.py
@@ -0,0 +1,2 @@
+#esse execicio era para coloar musica mp3 porem eu cho desnecesario fazer isso no python por causa que existem outras ferramentas para fazer isso 
+#então para não ficar desorgranizao vou pular esse 
