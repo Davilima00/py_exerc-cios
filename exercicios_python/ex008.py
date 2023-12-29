bd123a5027ceb7e08c2a1250d45fd1cd706b5109
Davi <davialvesdeelimaa@gmail.com>
2023-12-28 23:40:50 -0300

    adicionando arquivos

---
+++ exercicios_python/ex008.py
@@ -0,0 +1,2 @@
+distancia = float(input('uma distrancia em metros '))
+print(f'A medida de {distancia} corresponde a \n{distancia / 1000} km \n{distancia / 100:} hm \n{distancia / 10} dam \n{distancia *10:.0f} dm \n{distancia * 100:.0f} cm \n{distancia *1000:.0f} mm')
