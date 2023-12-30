2aad68e376fa793ab607818aea511c05a2e7bc73
Davi <davialvesdeelimaa@gmail.com>
2023-12-30 20:48:29 -0300

    calculando valor da passagem

---
+++ exercicios_python/ex031.py
@@ -0,0 +1,5 @@
+distancia = int(input('qual a distancia da sua viagem '))
+if distancia > 200:
+    print(f'o valor da sua passagem foi {distancia * 0.45}')
+else:
+    print(f'o valor a dua passagem foi {distancia * 0.50}')
