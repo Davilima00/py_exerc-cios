426552129be1dc1305af7edbfb29f3ff766ceb0b
Davi <davialvesdeelimaa@gmail.com>
2023-12-31 11:20:41 -0300

    emprestimo

---
+++ exercicios_python/ex036.py
@@ -0,0 +1,11 @@
+casa = int(input('digite o valor da casa '))
+salario = int(input('digite o valor do seu salario '))
+prestacao = int(input('quantos anos '))
+valorpres = casa / prestacao
+valorsala = (salario / (100/30))
+print(f'para pagar uma casa de {casa} a prestaçao vai ser de {valorpres}')
+if casa <= valorsala:
+    print('seu emprestimo foi consebido')
+else.:
+    print('emprestimo negado')
+
