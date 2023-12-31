556d09a6e0c19f887968597e68dc86fd907bc635
Davi <davialvesdeelimaa@gmail.com>
2023-12-31 11:36:22 -0300

    maior ou menor

---
+++ exercicios_python/ex038.py
@@ -0,0 +1,8 @@
+numero1 = int(input('digite um numero \n'))
+numero2 = int(input('digite um numero \n'))
+if numero1 < numero2:
+    print('o segundo numero e maior')
+elif numero1 == numero2:
+    print('os dois numeros são iguais')
+else:
+    print('o primeiro numero e maior')
