79b4cfae841de2792c9284bc25b28ac2cae5afcb
Davi <davialvesdeelimaa@gmail.com>
2024-01-02 11:05:21 -0300

    soma dos pares

---
+++ exercicios_python/ex050.py
@@ -0,0 +1,11 @@
+soma = 0
+
+for num in range(0, 6):
+    numero = int(input('digite um numero '))
+    if numero % 2 == 0:
+        soma += numero
+        
+print(f'a soma entre todos os pares foram {soma}')
+
+# ive que repetir esse pois acabei me perdendo
+# então como ia dar um trabanho para arrumar so continuei 
