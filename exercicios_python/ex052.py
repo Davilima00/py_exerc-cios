c3e01a7c3dbf2d5f443387a78bdf0edd20fdb078
Davi <davialvesdeelimaa@gmail.com>
2024-01-02 19:28:16 -0300

    exeicios python beecrowd

---
+++ exercicios_python/ex052.py
@@ -0,0 +1,8 @@
+numero = int(input('digite um numero para saber se ele e primo: '))
+soma = 0
+
+for cont in range(2, numero):
+    if cont % numero == 0
+        soma += 1
+        print(soma)
+    
