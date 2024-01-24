2c5e0538b8cc93f1e35df6942152b19b5758b5e1
Davi <davialvesdeelimaa@gmail.com>
2024-01-24 16:43:40 -0300

    adicionando fucionalidade

--- exercicios_python/funcoes.py/matematica.py
+++ exercicios_python/funcoes.py/matematica.py
@@ -1,2 +1,25 @@
 # metade dobro almento
 
+def metade(numero):
+    """param numero a entrata de qualquer numero
+    retun a saida da metade """
+    return numero / 2
+
+
+def dobro(numero):
+    """param numero a entrada
+    return a saida do dobro"""
+    return numero * 2
+
+
+def tripo(numero):
+    """param numero a entrada
+    return a saida do tripo"""
+    return numero * 3
+
+
+def almento(numero):
+    """param numero a entrada
+    return a saida com 10 pocento de almento"""
+    return numero * (10/100)
+
